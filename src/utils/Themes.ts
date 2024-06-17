2cf7d3819e45e07706fc5eb79cc2508de5853e58
Abhiraj-cuchd <21bca1058@cuchd.in>
2024-06-17 17:14:07 +0530

    misc

--- src/utils/Themes.ts
+++ src/utils/Themes.ts
@@ -20,3 +20,10 @@ export const lightTheme = {
   card: "#FFFFFF",
   button: "#5c5b5b",
 };
+
+// "rewrites": [
+    //     {
+    //         "source": "/(.*)",
+    //         "destination": "/"
+    //     }
+    // ]
